d56d0897abc590fd4343121835b17f17e1c4d650
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-03-23 13:19:36 -0500

    fixed first commit

--- db/index.js
+++ db/index.js
@@ -3,6 +3,20 @@ const { Client } = require('pg'); // imports the pg module
 // supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(`
+    INSERT INTO users(username, password)
+    VALUES ($1, $2)
+    ON CONFLICT (username) DO NOTHING 
+    RETURNING *;
+  `, [username, password]);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
 async function getAllUsers() {
     const { rows } = await client.query(
       `SELECT id, username 
@@ -12,20 +26,6 @@ async function getAllUsers() {
     return rows;
   }
 
-  async function createUser({ username, password }) {
-    try {
-      const result = await client.query(`
-      INSERT INTO users(username, password)
-      VALUES ($1, $2)
-      ON CONFLICT (username) DO NOTHING 
-      RETURNING *;
-    `, [username, password]);
-  
-      return result;
-    } catch (error) {
-      throw error;
-    }
-  }
 
 
 
--- db/seed.js
+++ db/seed.js
@@ -1,35 +1,6 @@
 // grab our client with destructuring from the export in index.js
 const { client, getAllUsers, createUser } = require('./index');
 
-async function createInitialUsers() {
-    try {
-        console.log("Starting to create users...");
-        
-        const albert = await createUser({ username: 'albert', password: 'bertie99' });
-        const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
-        const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
-
-        console.log(albert);
-        
-        console.log("Finished creating users!");
-    } catch(error) {
-        console.error("Error creating users!");
-        throw error;
-    }
-}
-
-async function rebuildDB() {
-    try {
-        client.connect();
-        
-        await dropTables();
-        await createTables();
-        await createInitialUsers();
-    } catch (error) {
-        throw error;
-    } 
-    
-}
 
 async function dropTables() {
     try {
@@ -61,6 +32,35 @@ async function createTables() {
         }
     }
     
+    async function createInitialUsers() {
+        try {
+            console.log("Starting to create users...");
+            
+            const albert = await createUser({ username: 'albert', password: 'bertie99' });
+            const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
+            const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
+    
+            console.log(albert);
+            
+            console.log("Finished creating users!");
+        } catch(error) {
+            console.error("Error creating users!");
+            throw error;
+        }
+    }
+    
+    async function rebuildDB() {
+        try {
+            client.connect();
+            
+            await dropTables();
+            await createTables();
+            await createInitialUsers();
+        } catch (error) {
+            throw error;
+        } 
+        
+    }
     async function testDB(){
         try{
             console.log("Starting to test database...")
