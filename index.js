0fe7dadcda4e50b7a7ef3e0d53bac9fa7ea24449
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-03-28 11:11:08 -0500

    part 2.1 api posts

---
+++ index.js
@@ -0,0 +1,25 @@
+const PORT = 3000;
+const express = require('express');
+const server = express();
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.json())
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
