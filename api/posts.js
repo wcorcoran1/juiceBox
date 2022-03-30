57186ca86e6575b625248b710b548379031b3b6e
coryla <cory.lance.anderson@gmail.com>
2022-03-30 13:35:49 -0500

    starting writing PATCH

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require("../db");
+const { requireUser } = require("./utils");
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -8,12 +9,38 @@ postsRouter.use((req, res, next) => {
   next();
 });
 
-postsRouter.get('/', async (req, res) =>{
-    const posts = await getAllPosts();
-    
-    res.send({
-        posts
-    });
+postsRouter.get("/", async (req, res) => {
+  const posts = await getAllPosts();
+
+  res.send({
+    posts,
+  });
+});
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+    // add authorId, title, content to postData object
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object
+    res.send({ post });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = postsRouter;
--- db/seed.js
+++ db/seed.js
@@ -49,7 +49,7 @@ async function createTables() {
           );
             CREATE TABLE posts(
             id SERIAL PRIMARY KEY,
-            "authorId" INTEGER REFERENCES users(id),
+            "" INTEGER REFERENCES users(id),
             title VARCHAR(255) NOT NULL,
             content TEXT NOT NULL,
             active BOOLEAN DEFAULT true
@@ -111,7 +111,7 @@ async function createInitialPosts() {
 
     console.log("Starting to create posts...");
     await createPost({
-      authorId: albert.id,
+      : albert.id,
       title: "First Post",
       content:
         "This is my first post. I hope I love writing blogs as much as I love writing them.",
@@ -119,14 +119,14 @@ async function createInitialPosts() {
     });
 
     await createPost({
-      authorId: sandra.id,
+      : sandra.id,
       title: "How does this work?",
       content: "Seriously, does this even do anything?",
       tags: ["#happy", "#worst-day-ever"],
     });
 
     await createPost({
-      authorId: glamgal.id,
+      : glamgal.id,
       title: "Living the Glam Life",
       content: "Do you even? I swear that half of you are posing.",
       tags: ["#happy", "#youcandoanything", "#canmandoeverything"],
