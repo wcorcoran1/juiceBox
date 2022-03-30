97b52475f8425a324ba31e796aae921e97cc0689
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-03-29 21:27:50 -0500

    token from thunderclent

--- api/users.js
+++ api/users.js
@@ -4,6 +4,7 @@ const { getAllUsers, getUserByUsername } = require('../db');
 const jwt = require('jsonwebtoken')
 
 
+
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
 
