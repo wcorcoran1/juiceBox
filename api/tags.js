b108a77e873d7bfbd47e4c5ac7cc58ba58e04dea
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-03-30 14:11:41 -0500

    working on tagsName posts api

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -14,6 +14,23 @@ tagsRouter.get('/', async (req, res) =>{
     res.send({
         tags
     });
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+
+  try {
+   const tagName = await getPostsByTagName();
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+
+      res.send({
+        posts:tagName
+      })
+  } catch ({ name, message }) {
+    next({ name, message })
+    // forward the name and message to the error handler
+  }
 });
 
 module.exports = tagsRouter;
