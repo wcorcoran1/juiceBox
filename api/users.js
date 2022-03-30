0263a6a5b1cfda295d893922f95728aaa38fb7c8
coryla <cory.lance.anderson@gmail.com>
2022-03-30 10:53:37 -0500

    finished part 2

--- api/users.js
+++ api/users.js
@@ -1,9 +1,7 @@
-const express = require('express');
+const express = require("express");
 const usersRouter = express.Router();
-const { getAllUsers, getUserByUsername } = require('../db');
-const jwt = require('jsonwebtoken')
-
-
+const { getAllUsers, getUserByUsername, createUser } = require("../db");
+const jwt = require("jsonwebtoken");
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -11,22 +9,22 @@ usersRouter.use((req, res, next) => {
   next();
 });
 
-usersRouter.get('/', async (req, res) =>{
-    const users = await getAllUsers()
-    
-    res.send({
-        users
-    });
+usersRouter.get("/", async (req, res) => {
+  const users = await getAllUsers();
+
+  res.send({
+    users,
+  });
 });
 
-usersRouter.post('/login', async (req, res, next) => {
+usersRouter.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
-  
+
   // request must have both
   if (!username || !password) {
     next({
       name: "MissingCredentialsError",
-      message: "Please supply both a username and password"
+      message: "Please supply both a username and password",
     });
   }
 
@@ -35,18 +33,61 @@ usersRouter.post('/login', async (req, res, next) => {
 
     if (user && user.password == password) {
       // create token & return to user
-      const token = jwt.sign({id:user.id, username:username}, process.env.JWT_SECRET)
-      res.send({ message: "you're logged in!", 
-      token: token,})
+      const token = jwt.sign(
+        { id: user.id, username: username },
+        process.env.JWT_SECRET
+      );
+      res.send({ message: "you're logged in!", token: token });
     } else {
-      next({ 
-        name: 'IncorrectCredentialsError', 
-        message: 'Username or password is incorrect'
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
       });
     }
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     next(error);
   }
 });
+
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists",
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
+
+    res.send({
+      message: "thank you for signing up",
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
 module.exports = usersRouter;
