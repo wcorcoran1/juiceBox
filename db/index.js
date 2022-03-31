d03b59264181bd9ed6811b9d8a099dbb61bdb402
coryla <cory.lance.anderson@gmail.com>
2022-03-31 12:52:52 -0500

    updated2

--- db/index.js
+++ db/index.js
@@ -1,9 +1,14 @@
 const { Client } = require("pg"); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client(
-  process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev"
-);
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 async function createUser({ username, password, name, location }) {
   try {
