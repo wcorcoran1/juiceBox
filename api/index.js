5a32acd37b6a7e7544d9c7ba8c67c1da1a64f85c
coryla <cory.lance.anderson@gmail.com>
2022-03-31 09:34:59 -0500

    starting deployment to heroku

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ apiRouter.use(async (req, res, next) => {
 
 apiRouter.use((req, res, next) => {
   if (req.user) {
-    console.log("User is set:", req.user);
+    console.log("User is set:", req.user.posts[0].tags);
   }
 
   next();
